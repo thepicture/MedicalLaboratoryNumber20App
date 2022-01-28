a00a12ccebb6157dafdaf8fbd6dafd54bcae9d28
thepicture <85333076+thepicture@users.noreply.github.com>
2022-01-28 14:31:04 +0000

    Design LoginPage

--- Views/Windows/NavigationWindow.xaml.cs
+++ Views/Windows/NavigationWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using MedicalLaboratoryNumber20App.Views.Pages;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace MedicalLaboratoryNumber20App
 {
@@ -7,9 +9,17 @@ namespace MedicalLaboratoryNumber20App
     /// </summary>
     public partial class NavigationWindow : Window
     {
+        public string CurrentTitle => (MainFrame.Content as Page)?.Title;
         public NavigationWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+
+            if (!MainFrame.Navigate(new LoginPage()))
+            {
+                MessageBox.Show("Не удалось запустить приложение");
+            }
         }
     }
 }
