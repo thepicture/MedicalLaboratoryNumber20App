ed7613b6c817979705b3f5e3f5838fb3e2a951fd
thepicture <85333076+thepicture@users.noreply.github.com>
2022-01-31 08:02:32 +0000

    Design LoginHistoryPage

--- Views/Pages/AdminPages/AdminPage.xaml.cs
+++ Views/Pages/AdminPages/AdminPage.xaml.cs
@@ -12,5 +12,10 @@ namespace MedicalLaboratoryNumber20App.Views.Pages.AdminPages
             InitializeComponent();
             DataContext = this;
         }
+
+        private void NavigateToLoginHistories(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _ = NavigationService.Navigate(new LoginHistoryPage());
+        }
     }
 }
---
+++ Views/Pages/AdminPages/LoginHistoryPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MedicalLaboratoryNumber20App.Models.Entities;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Windows.Controls;
+
+namespace MedicalLaboratoryNumber20App.Views.Pages.AdminPages
+{
+    /// <summary>
+    /// Interaction logic for LoginHistoryPage.xaml
+    /// </summary>
+    public partial class LoginHistoryPage : Page
+    {
+        public LoginHistoryPage()
+        {
+            InitializeComponent();
+            IList<string> sortTypes = new List<string>
+            {
+                "Без сортировки",
+                "По возрастанию",
+                "По убыванию",
+            };
+            ComboSort.ItemsSource = sortTypes;
+            DataContext = this;
+        }
+        private async void PerformSearchHistories()
+        {
+            using (MedicalLaboratoryNumber20Entities context = new MedicalLaboratoryNumber20Entities())
+            {
+                LoginHistories.ItemsSource = await context.LoginHistory.ToListAsync();
+            }
+        }
+
+        private void OnLoginSearchTextChanged(object sender, TextChangedEventArgs e)
+        {
+            PerformSearchHistories();
+        }
+
+        private void OnSortChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PerformSearchHistories();
+        }
+    }
+}
