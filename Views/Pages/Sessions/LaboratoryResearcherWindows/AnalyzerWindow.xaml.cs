f81cab1baca8521832afddc015a19afd5562bd51
thepicture <85333076+thepicture@users.noreply.github.com>
2022-02-07 10:03:08 +0000

    Add updating a status in the database after successful post request

--- Views/Pages/Sessions/LaboratoryResearcherWindows/AnalyzerWindow.xaml.cs
+++ Views/Pages/Sessions/LaboratoryResearcherWindows/AnalyzerWindow.xaml.cs
@@ -3,6 +3,7 @@ using MedicalLaboratoryNumber20App.Models.Entities;
 using MedicalLaboratoryNumber20App.Models.Services;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.Entity;
 using System.Linq;
 using System.Net.Http;
@@ -30,7 +31,12 @@ namespace MedicalLaboratoryNumber20App.Views.Pages.Sessions.LaboratoryResearcher
         /// <summary>
         /// Подгружает невыполненные услуги асинхронно.
         /// </summary>
-        private async void LoadUnperfomedServicesAsync()
+        private async 
+        /// <summary>
+        /// Подгружает невыполненные услуги асинхронно.
+        /// </summary>
+        Task
+LoadUnperfomedServicesAsync()
         {
             IEnumerable<BloodServiceOfUser> bloodServices =
                 await Task.Run(() =>
@@ -39,6 +45,7 @@ namespace MedicalLaboratoryNumber20App.Views.Pages.Sessions.LaboratoryResearcher
                     new MedicalLaboratoryNumber20Entities())
                     {
                         return context.BloodServiceOfUser
+                        .Where(bs => bs.IsAccepted)
                         .Include(bs => bs.Blood)
                         .Include(bs => bs.Blood.Patient)
                         .Include(bs => bs.BloodStatus)
@@ -69,7 +76,7 @@ namespace MedicalLaboratoryNumber20App.Views.Pages.Sessions.LaboratoryResearcher
                     {
                         new SimpleService
                         {
-                            ServiceCode = service.ServiceCode,
+                            ServiceCode = service.Service.Code,
                         }
                     }.ToArray(),
                 };
@@ -100,8 +107,23 @@ namespace MedicalLaboratoryNumber20App.Views.Pages.Sessions.LaboratoryResearcher
                         }
                         else
                         {
+                            await Task.Run(() =>
+                            {
+                                using (MedicalLaboratoryNumber20Entities context =
+                                new MedicalLaboratoryNumber20Entities())
+                                {
+                                    BloodServiceOfUser databaseBloodService =
+                                    context.BloodServiceOfUser
+                                    .Find(new object[] { service.BloodId, service.ServiceCode });
+                                    databaseBloodService.BloodStatus =
+                                    context.BloodStatus
+                                    .First(b => b.BloodStatusName == "Отправлена на исследование");
+                                    _ = context.SaveChanges();
+                                }
+                            });
                             button.IsEnabled = false;
                             button.Content = "Отправлена";
+                            await LoadUnperfomedServicesAsync();
                             MessageBoxService.ShowInfo($"Услуга успешно отправлена");
                         }
                     }
@@ -111,6 +133,13 @@ namespace MedicalLaboratoryNumber20App.Views.Pages.Sessions.LaboratoryResearcher
                     MessageBoxService.ShowError("Не удалось отправить услугу. " +
                         "Прошло максимальное время ожидания в 30 секунд. " +
                         "Проверьте и исправьте настройки сети, затем повторите попытку");
+                    System.Diagnostics.Debug.WriteLine(ex.StackTrace);
+                }
+                catch (DbException ex)
+                {
+                    MessageBoxService.ShowError("Не удалось отправить услугу. " +
+                        "Возникла проблема с обновлением статуса в базе данных. " +
+                        "Проверьте подключение к базе данных и повторите попытку");
                     System.Diagnostics.Debug.WriteLine(ex.StackTrace);
                 }
             });
