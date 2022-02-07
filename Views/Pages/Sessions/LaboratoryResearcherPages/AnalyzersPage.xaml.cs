bdf601884cb15ea1bf95082165048a5ea119d6bd
thepicture <85333076+thepicture@users.noreply.github.com>
2022-02-07 08:02:45 +0000

    Add button disabling on navigate to AnalyzerWindow

--- Views/Pages/Sessions/LaboratoryResearcherPages/AnalyzersPage.xaml.cs
+++ Views/Pages/Sessions/LaboratoryResearcherPages/AnalyzersPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using MedicalLaboratoryNumber20App.Models.Entities;
+using MedicalLaboratoryNumber20App.Views.Pages.Sessions.LaboratoryResearcherWindows;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace MedicalLaboratoryNumber20App.Views.Pages.Sessions.LaboratoryResearcherPages
@@ -34,6 +36,28 @@ namespace MedicalLaboratoryNumber20App.Views.Pages.Sessions.LaboratoryResearcher
                 }
             });
             Analyzers.ItemsSource = currentAnalyzers;
+        }
+
+        /// <summary>
+        /// Осуществляет открытие окна анализатора.
+        /// </summary>
+        private void PerformAnalyzerWindowOpen(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            string buttonOldContent = button.Content.ToString();
+            Analyzer analyzer = button.DataContext as Analyzer;
+            AnalyzerWindow analyzerWindow = new AnalyzerWindow(analyzer)
+            {
+                Owner = App.Current.MainWindow
+            };
+            analyzerWindow.Closed += (window, args) =>
+            {
+                button.Content = buttonOldContent;
+                button.IsEnabled = true;
+            };
+            button.Content = "Окно уже открыто";
+            button.IsEnabled = false;
+            analyzerWindow.Show();
         }
     }
 }
---
+++ Views/Pages/Sessions/LaboratoryResearcherWindows/AnalyzerWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MedicalLaboratoryNumber20App.Models.Entities;
+using System.Windows;
+
+namespace MedicalLaboratoryNumber20App.Views.Pages.Sessions.LaboratoryResearcherWindows
+{
+    /// <summary>
+    /// Interaction logic for AnalyzerWindow.xaml
+    /// </summary>
+    public partial class AnalyzerWindow : Window
+    {
+        public AnalyzerWindow(Analyzer analyzer)
+        {
+            InitializeComponent();
+            Analyzer = analyzer;
+            DataContext = this;
+        }
+
+        public Analyzer Analyzer { get; private set; }
+    }
+}
