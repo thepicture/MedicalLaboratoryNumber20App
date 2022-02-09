08a4971ab03954b2186b775569f7f0f8b01afb9e
thepicture <85333076+thepicture@users.noreply.github.com>
2022-02-09 09:32:57 +0000

    Add insurance companies report generation

---
+++ Services/InsuranceCompanyReportService.cs
@@ -0,0 +1,118 @@
+﻿using MedicalLaboratoryNumber20App.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace MedicalLaboratoryNumber20App.Services
+{
+    public class InsuranceCompanyReportService :
+        IReportService<IEnumerable<InsuranceCompany>>
+    {
+        private readonly DateTime? _from;
+        private readonly DateTime? _to;
+
+        public InsuranceCompanyReportService(DateTime? from,
+                                             DateTime? to)
+        {
+            _from = from;
+            _to = to;
+        }
+
+        public bool GenerateReport(IEnumerable<InsuranceCompany> obj,
+                                   string path)
+        {
+            Excel.Application application = null;
+            Excel.Workbook workbook = null;
+            try
+            {
+                application = new Excel.Application();
+
+                workbook = application.Workbooks.Add();
+                Excel.Worksheet sheet = workbook.Sheets.Add();
+
+                int rowCounter = 1;
+                decimal totalSum = 0;
+                Excel.Range range;
+                for (int i = 0; i < obj.Count(); i++)
+                {
+                    Excel.Range insuranceCompanyRangeStart = sheet.Cells[1][rowCounter];
+                    range = sheet.Range[sheet.Cells[1][rowCounter], sheet.Cells[2][rowCounter]];
+                    range.Merge();
+                    range.Value = "Отчёт на компанию " + obj.ElementAt(i).InsuranceName;
+                    range.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                    rowCounter++;
+                    sheet.Cells[1][rowCounter] = "Название страховой компании";
+                    sheet.Cells[2][rowCounter] = obj.ElementAt(i).InsuranceName;
+                    rowCounter++;
+                    sheet.Cells[1][rowCounter] = "Период для оплаты";
+                    sheet.Cells[2][rowCounter] = $"С {_from:yyyy/MM/dd} "
+                                                 + $"по {_to:yyyy/MM/dd}";
+                    rowCounter++;
+                    range = sheet.Range[sheet.Cells[1][rowCounter], sheet.Cells[2][rowCounter]];
+                    range.Merge();
+                    range.Value = "ФИО пациентов с оказанными им услугами ";
+                    range.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                    rowCounter++;
+                    foreach (Patient patient in obj.ElementAt(i).Patient)
+                    {
+                        range = sheet.Range[sheet.Cells[1][rowCounter], sheet.Cells[2][rowCounter]];
+                        range.Merge();
+                        range.Value = $"Пациент {patient.PatientFullName}";
+                        range.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                        rowCounter++;
+
+                        decimal currentPatientSum = 0;
+
+                        foreach (Blood blood in patient.Blood)
+                        {
+                            foreach (BloodServiceOfUser bloodService in blood.BloodServiceOfUser)
+                            {
+                                sheet.Cells[1][rowCounter] = bloodService.Service.ServiceName;
+                                sheet.Cells[2][rowCounter] = $"{bloodService.Service.PriceInRubles:F2} руб.";
+                                rowCounter++;
+                                currentPatientSum += bloodService.Service.PriceInRubles;
+                                totalSum += bloodService.Service.PriceInRubles;
+                            }
+                        }
+                        range = sheet.Range[sheet.Cells[1][rowCounter], sheet.Cells[2][rowCounter]];
+                        range.Merge();
+                        range.Value = $"Общая стоимость услуг пациента: {currentPatientSum:F2} руб.";
+                        range.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                    }
+                    Excel.Range allReportRange = sheet.Range[insuranceCompanyRangeStart, sheet.Cells[2][rowCounter]];
+                    allReportRange.Borders[Excel.XlBordersIndex.xlEdgeTop].LineStyle =
+                        allReportRange.Borders[Excel.XlBordersIndex.xlEdgeRight].LineStyle =
+                        allReportRange.Borders[Excel.XlBordersIndex.xlEdgeBottom].LineStyle =
+                        allReportRange.Borders[Excel.XlBordersIndex.xlEdgeLeft].LineStyle =
+                        allReportRange.Borders[Excel.XlBordersIndex.xlInsideHorizontal].LineStyle =
+                        allReportRange.Borders[Excel.XlBordersIndex.xlInsideVertical].LineStyle =
+                            Excel.XlLineStyle.xlContinuous;
+                    rowCounter += 2;
+                }
+                range = sheet.Range[sheet.Cells[1][rowCounter], sheet.Cells[2][rowCounter]];
+                range.Merge();
+                range.Value = $"Итоговая стоимость по всем пациентам: {totalSum:F2} руб.";
+                range.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+
+                sheet.Columns.AutoFit();
+
+                sheet.SaveAs(Path.Combine(path, "savedsheet.csv"), Excel.XlFileFormat.xlCSV);
+                sheet.ExportAsFixedFormat(Excel.XlFixedFormatType.xlTypePDF, Path.Combine(path, "savedsheet.pdf"));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.StackTrace);
+                return false;
+                throw;
+            }
+            finally
+            {
+                workbook?.Close(false);
+                application?.Quit();
+            }
+        }
+    }
+}
--- Views/Pages/AccountantPages/AccountantPage.xaml.cs
+++ Views/Pages/AccountantPages/AccountantPage.xaml.cs
@@ -1,4 +1,14 @@
-﻿using System.Windows.Controls;
+﻿using MedicalLaboratoryNumber20App.Models.Entities;
+using MedicalLaboratoryNumber20App.Models.Services;
+using MedicalLaboratoryNumber20App.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
 
 namespace MedicalLaboratoryNumber20App.Views.Pages.AccountantPages
 {
@@ -11,6 +21,101 @@ namespace MedicalLaboratoryNumber20App.Views.Pages.AccountantPages
         {
             InitializeComponent();
             DataContext = this;
+        }
+
+        /// <summary>
+        /// Формирует счёт страховой компании в формате .pdf и .csv.
+        /// </summary>
+        private async void PerformGenerateOrder(object sender,
+                                                RoutedEventArgs e)
+        {
+            if (FromPicker.SelectedDate == null
+                || ToPicker.SelectedDate == null)
+            {
+                _ = MessageBoxService.ShowWarning("Укажите " +
+                    "корректную дату " +
+                    "начала и окончания периода");
+                return;
+            }
+            if (FromPicker.SelectedDate >= ToPicker.SelectedDate)
+            {
+                _ = MessageBoxService
+                    .ShowWarning("Дата начала периода "
+                                 + "должна быть " +
+                                 "раньше даты " +
+                                 "окончания периода");
+                return;
+            }
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog
+            {
+                Description = "Укажите путь " +
+                "к папке " +
+                "с сохраненными отчётами"
+            };
+
+            if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            GenerateOrderButton.IsEnabled = false;
+            string buttonOldContent = GenerateOrderButton.Content.ToString();
+            GenerateOrderButton.Content = "Формируется отчёт ...";
+
+            DateTime? from = FromPicker.SelectedDate;
+            DateTime? to = ToPicker.SelectedDate;
+
+            IEnumerable<InsuranceCompany> companies = await Task.Run(() =>
+            {
+                using (MedicalLaboratoryNumber20Entities context =
+                new MedicalLaboratoryNumber20Entities())
+                {
+                    return context.InsuranceCompany
+                    .Include(c => c.Patient)
+                    .Include(c => c.Patient.Select(p => p.Blood))
+                    .Include(c => c.Patient.Select(p => p.Blood.Select(b => b.BloodServiceOfUser)))
+                    .Include(c => c.Patient.Select(p => p.Blood.Select(b => b.BloodServiceOfUser.Select(bs => bs.Service))))
+                    .Where(c => Enumerable.All(c.Patient.Select(p => p.Blood.Select(b => b.BloodServiceOfUser.Select(bs => bs.FinishedDateTime))), d1 => d1.All(d2 => d2.All(d3 => d3 < to && d3 > from))))
+                    .ToList();
+                }
+            });
+            if (companies.Count() == 0)
+            {
+                _ = MessageBoxService.ShowWarning("За указанный период не найдено услуг");
+                return;
+            }
+            if (companies.Select(c => c.Patient).Distinct().Count() > 500)
+            {
+                if (!MessageBoxService.ShowQuestion("За указанный период " +
+                    "найдено более 500 пациентов. Формирование отчёта может занять " +
+                    "долгое время. Продолжить?"))
+                {
+                    return;
+                }
+            }
+
+            InsuranceCompanyReportService reportService =
+                new InsuranceCompanyReportService(FromPicker.SelectedDate,
+                                                  ToPicker.SelectedDate);
+            bool isSuccessfulReporting = await Task.Run(() =>
+            {
+                return reportService.GenerateReport(companies,
+                                                    folderBrowserDialog.SelectedPath);
+            });
+            if (isSuccessfulReporting)
+            {
+                MessageBoxService.ShowInfo("Отчёт успешно сгенерирован " +
+                    $"по пути {folderBrowserDialog.SelectedPath}");
+            }
+            else
+            {
+                MessageBoxService.ShowError("Отчёт не сгенерирован. " +
+                    $"убедитесь, что путь {folderBrowserDialog.SelectedPath} " +
+                    "существует и попробуйте ещё раз");
+            }
+
+            GenerateOrderButton.Content = buttonOldContent;
+            GenerateOrderButton.IsEnabled = true;
         }
     }
 }
